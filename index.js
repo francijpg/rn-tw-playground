e50b4199c339505ddc6af232b55f108894d963ee
Manuel Goncalves <man281192@gmail.com>
2022-04-13 12:49:04 -0400

    chore:changes about platforms files

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
+import { name as appName } from './app.json';
+import Debug from './src/domain/user/components/Debug';
 import Login from './src/domain/user/components/Login';
-import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => Login);
+const IS_DEBUG = false;
+AppRegistry.registerComponent(appName, () => (IS_DEBUG ? Debug : Login));
