16de5b54c37426c566d4c7c06ad103518cd7fb69
Manuel Goncalves <man281192@gmail.com>
2022-04-13 12:49:36 -0400

    chore: adding custom button and debug screen

---
+++ src/domain/user/components/Debug.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+const Debug = () => {
+  return <View style={styles.base} />;
+};
+
+const styles = StyleSheet.create({
+  base: {},
+});
+
+export default Debug;
