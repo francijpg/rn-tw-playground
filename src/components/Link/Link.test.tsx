30b2ca121430d377dfb45c409dcd815f58fb59d9
marlaurita <marychina2@gmail.com>
2022-04-14 08:47:00 -0400

    chore: using props in Link Component

--- src/components/Link/Link.test.tsx
+++ src/components/Link/Link.test.tsx
@@ -5,7 +5,9 @@ import { render } from '@testing-library/react-native';
 describe('Testing Link Component', () => {
   it('should render a text', () => {
     // arrange
-    const wrapper = render(<Link />);
+    const wrapper = render(
+      <Link url="https://google.com" text="Olvidaste tu password?" />,
+    );
 
     // act
     const text = wrapper.getByText('Olvidaste tu password?');
