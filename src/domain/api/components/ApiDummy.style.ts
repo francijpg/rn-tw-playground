0bb722ee9beecf4853076c061ee78beccb63b399
Francisco PG <hellofrancijpg@gmail.com>
2022-04-20 08:41:02 -0400

    Update ApiDummy.style.ts

--- src/domain/api/components/ApiDummy.style.ts
+++ src/domain/api/components/ApiDummy.style.ts
@@ -3,16 +3,43 @@ import { StyleSheet } from 'react-native';
 const styles = StyleSheet.create({
   container: {
     marginTop: 50,
-    flex: 1,
-  },
-  image: {
-    width: 60,
-    height: 60,
-    marginBottom: 10,
   },
   containerImage: {
-    margin: 10,
-    flexDirection: 'column',
+    width: '90%',
+    alignSelf: 'center',
+    borderRadius: 40,
+    shadowOpacity: 0.5,
+    shadowColor: '#000',
+    shadowOffset: {
+      height: 5,
+      width: 5,
+    },
+    backgroundColor: '#fff',
+    marginTop: 20,
+  },
+  image: {
+    height: 200,
+    width: '100%',
+    borderTopLeftRadius: 40,
+    borderTopRightRadius: 40,
+  },
+  cardContent: {
+    padding: 20,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: '600',
+    marginTop: 10,
+  },
+  data: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 10,
+  },
+  source: {
+    color: '#e63946',
+    fontWeight: 'bold',
+    fontSize: 12,
   },
 });
 
