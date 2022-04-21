4c26aa5ef4ccac20a2f2799fe458efaddb79f522
marlaurita <marychina2@gmail.com>
2022-04-20 20:38:21 -0400

    chore:navigation initial implementation

--- src/assets/string.tsx
+++ src/assets/string.tsx
@@ -3,6 +3,11 @@ export class PlatformStr {
   static readonly ANDROID = 'android';
 }
 
+export class RoutesStr {
+  static readonly LOGIN_SCREEN = 'Login';
+  static readonly API_SCREEN = 'Api';
+}
+
 export class Strings {
   static readonly FORGOT_PASS = 'Olvidaste tu password?';
 }
