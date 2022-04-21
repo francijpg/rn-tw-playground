c99ed763648aa1b60a88032ab7ecbe7c842a59de
Manuel Goncalves <man281192@gmail.com>
2022-04-21 16:21:27 -0400

    chore: adding font weight to all fonts

--- src/infraestructure/theme/fonts.tsx
+++ src/infraestructure/theme/fonts.tsx
@@ -3,12 +3,15 @@ import { Fonts } from 'react-native-paper/lib/typescript/types';
 const _fonts: Fonts = {
   regular: {
     fontFamily: 'lato-regular',
+    fontWeight: 'normal',
   },
   medium: {
     fontFamily: 'lato-regular',
+    fontWeight: 'normal',
   },
   light: {
     fontFamily: 'lato-regular',
+    fontWeight: 'normal',
   },
   thin: {
     fontFamily: 'lato-regular',
