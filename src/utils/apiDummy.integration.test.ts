fbb180afd11e28e138886196746bf4769a5d37a3
Francisco PG <hellofrancijpg@gmail.com>
2022-04-22 09:07:50 -0400

    tests in progress

---
+++ src/utils/apiDummy.integration.test.ts
@@ -0,0 +1,12 @@
+// import request from "supertest";
+import { REACT_APP_DUMMY_API } from '@env';
+
+const baseUrl = `${REACT_APP_DUMMY_API}/albums/1/photos`;
+
+describe('Todos endpoint', () => {
+  it.skip('should return a 200 status code', async () => {
+    // const response = await request(baseUrl)
+    // 	.get('/');
+    // expect(response.statusCode).toBe(200);
+  });
+});
