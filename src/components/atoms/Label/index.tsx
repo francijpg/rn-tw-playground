d24f4e736e2c394f56e031298554cbb964c7f7ef
Francisco PG <hellofrancijpg@gmail.com>
2022-04-26 09:45:54 -0400

    Create index.tsx

---
+++ src/components/atoms/Label/index.tsx
@@ -0,0 +1,20 @@
+import React, { memo } from 'react';
+import { StyleSheet, Text } from 'react-native';
+
+type Props = {
+  children: React.ReactNode;
+  style?: Object;
+};
+
+const Label = ({ children, style }: Props) => (
+  <Text style={[styles.text, style]}>{children}</Text>
+);
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 14,
+    color: '#000',
+  },
+});
+
+export default memo(Label);
